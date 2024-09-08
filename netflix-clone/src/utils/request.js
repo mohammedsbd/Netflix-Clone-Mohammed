e275364ea63525b79697ab7991008e460de4555f
mohammedsbd <Mohammed1112ok@gmail.com>
2024-09-08 04:15:08 -0700

    axios and requests

---
+++ netflix-clone/src/utils/request.js
@@ -0,0 +1,13 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOrginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?language=en-US&page=1&api_key=${API_KEY}`,
+  fetchActionMovies: `/discover/movie?with_genres=28&api_key=${API_KEY}`,
+  fetchComedyMovies: `/discover/movie?with_genres=35&api_key=${API_KEY}`,
+  fetchHorrorMovies: `/discover/movie?with_genres=27&api_key=${API_KEY}`,
+  fetchRomanceMovies: `/discover/movie?with_genres=10749&api_key=${API_KEY}`,
+  fetchDocumentaries: `/discover/movie?with_genres=99&api_key=${API_KEY}`,
+  fetchTvShows: `/tv/series_id?language=en-US&api_key=${API_KEY}`,
+};
+
