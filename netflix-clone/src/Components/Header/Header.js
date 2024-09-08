1f7eba988a71dea7b1a8853271a1c14aeb9ed3ee
mohammedsbd <Mohammed1112ok@gmail.com>
2024-09-08 00:31:35 -0700

    header and footer

---
+++ netflix-clone/src/Components/Header/Header.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import "./header.css"
+import NetflixLogo from "../../assests/images/NetflixLogo.png"
+import SearchIcon from "@mui/icons-material/Search"
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+const Header = () => {
+  return (
+    <div className="header_outer_container">
+      <div className="header-container">
+        <div className="header-left">
+          <ul>
+            <li>
+              <img src={NetflixLogo} alt="Netflix-logo" width="100" />
+            </li>
+            <li>Netflix</li>
+            <li>Home</li>
+            <li>TvShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyList</li>
+            <li>Browse by language</li>
+          </ul>
+        </div>
+        <div className="header-right">
+          <ul>
+            <li>
+              <SearchIcon />
+            </li>
+            <li><NotificationsNoneIcon/></li>
+            <li><AccountBoxIcon/></li>
+            <li><ArrowDropDownIcon/></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header
